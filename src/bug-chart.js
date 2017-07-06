b18ac9f0e206ad62cf2b36cbb7e6111c98f16e58
Jeremy Fry <jeremy@jeremyfry.com>
2017-07-05 20:49:24 -0400

    added some more styling. Playing with chart look/feel.

--- src/bug-chart.js
+++ src/bug-chart.js
@@ -1,23 +1,25 @@
 import * as d3 from 'd3';
-import styles from './index.css';
+import styles from './bug-chart.css';
 export default (chartSelector, data, chartOptions = {
 	width: 500,
 	height: 500,
 	margin: [20, 20, 20, 20]
 }) => {
-	const {width, height, margin} = chartOptions;
+	const {width, margin} = chartOptions;
 	const bugs = _formatData(data);
+	const height = bugs.length * 12;
 	const xScale = _createXScale(bugs, width);
 	const yScale = _createYScale(bugs, height);
-
-	const svg = _createChartSvg({chartSelector, height, width, margin, styles, scalingMethod: _responsivefy});
-
+	const chartSvg = _createChartSvg({chartSelector, height, width, margin, styles, scalingMethod: _responsivefy});
+    _addDropShadowFilter(chartSvg);
+	const svg = chartSvg.append('g')
+        .attr('transform', `translate(${margin[3]}, ${margin[0]})`);
 	svg
 		.append('g')
 			.attr('transform', `translate(0, ${height})`)
 			.call(d3.axisBottom(xScale));
 
-	_addElements({svg, yScale, xScale, bugs, styles})
+	_addBugElementsToChart({svg, yScale, xScale, bugs, styles})
 
 };
 
@@ -38,9 +40,8 @@ const _createChartSvg = (options) => {
 		.append('svg')
 		.attr('width', width+margin[1]+margin[3])
 		.attr('height', height+margin[0]+margin[2])
-		.call(scalingMethod)
-		.append('g')
-		.attr('transform', `translate(${margin[3]}, ${margin[0]})`);
+		.call(scalingMethod);
+
 };
 
 const _createXScale = (data, width) => {
@@ -58,58 +59,110 @@ const _createYScale = (data, height) => {
 		.range([0, height]);
 };
 
-const _addElements = (options) => {
+const _addBugElementsToChart = (options) => {
 	const {svg, yScale, xScale, bugs, styles} = options;
 	const items = svg.selectAll('.bug')
 		.data(bugs)
 		.enter()
 		.append('g')
-		.attr('class', 'bug')
+		.attr('class', styles.bug)
 		.attr('transform', (d, i) => {
 			return `translate(${xScale(d.created)}, ${yScale(i)})`;
-		});
+		})
+        .on('mouseover', function() {
+            d3.select(this)
+				.selectAll(function() { return this.childNodes; })
+				.style('filter', 'url(#drop-shadow)');
+        })
+        .on('mouseout', function() {
+            d3.select(this)
+                .selectAll(function() { return this.childNodes; })
+				.style('filter', 'none');
+        });
 
 	const width = d =>{
 		const resolved = d.resolved || xScale.domain()[1];
 		return Math.max(xScale(resolved) - xScale(d.created), 10);
 	};
+	const circleRadius = 4;
 
 	items.append('rect')
 		.attr('width', width)
-		.attr('height', 2);
+		.attr('height', 2)
+		.attr('class', styles.bugLine);
 
 	items.append('circle')
-		.attr('class', `${styles.open}`)
+		.attr('class', `${styles.bugEndPoint} ${styles.open}`)
 		.attr('cx', 0)
-		.attr('cy', 1)
-		.attr('r', 2);
+		.attr('cy', circleRadius/2)
+		.attr('r', circleRadius);
 
 	items.append('circle')
-		.attr('class', d => `${styles[d.resolution.toLowerCase().replace(/[\s']/g, '')]} ${d.resolution.toLowerCase().replace(/[\s']/g, '-')}`)
+		.attr('class', d => `${styles[d.resolution.toLowerCase().replace(/[\s']/g, '')]} ${d.resolution.toLowerCase().replace(/[\s']/g, '-')} ${styles.bugEndPoint}`)
 		.attr('cx', width)
-		.attr('cy', 1)
-		.attr('r', 2);
+		.attr('cy', circleRadius/2)
+		.attr('r', circleRadius);
 
 	items.append('div')
 		.attr('class', styles.tooltip)
 		.html('test');
 };
 
+const _showToolTip = () => {
+
+};
+
+const _hideToolTip = () => {
+
+};
+
+const _populateToolTip = () => {
+
+};
+
+const _addDropShadowFilter = (svg) => {
+    const defs = svg.append('defs');
+    const filter = defs.append('filter')
+        .attr('id', 'drop-shadow')
+        .attr('height', '200%')
+        .attr('y', '-50%')
+        .attr('width', '200%')
+        .attr('x', '-50%');
+
+    filter.append('feGaussianBlur')
+        .attr('in', 'SourceAlpha')
+        .attr('stdDeviation', 1.5)
+        .attr('result', 'blur');
+
+    filter.append('feOffset')
+        .attr('in', 'blur')
+        .attr('dx', 1)
+        .attr('dy', 1)
+        .attr('result', 'offsetBlur');
+
+    const feMerge = filter.append('feMerge');
+
+    feMerge.append('feMergeNode')
+        .attr('in', 'offsetBlur');
+    feMerge.append('feMergeNode')
+        .attr('in', 'SourceGraphic');
+};
+
 const _responsivefy = (svg) => {
 	const resize = () => {
-		var targetWidth = parseInt(container.style("width"));
-		svg.attr("width", targetWidth);
-		svg.attr("height", Math.round(targetWidth / aspect));
+		let targetWidth = parseInt(container.style('width'));
+		svg.attr('width', targetWidth);
+		svg.attr('height', Math.round(targetWidth / aspect));
 	};
 
 	const container = d3.select(svg.node().parentNode),
-		width = parseInt(svg.style("width")),
-		height = parseInt(svg.style("height")),
+		width = parseInt(svg.style('width')),
+		height = parseInt(svg.style('height')),
 		aspect = width / height;
 
-	svg.attr("viewBox", "0 0 " + width + " " + height)
-		.attr("preserveAspectRatio", "xMinYMid")
+	svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+		.attr('preserveAspectRatio', 'xMinYMid')
 		.call(resize);
 
-	d3.select(window).on("resize." + container.attr("id"), resize);
+	d3.select(window).on('resize.' + container.attr('id'), resize);
 };
